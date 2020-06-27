caaa85410db2089c11b55454371b649d564c7bc5
shjang7 <suhy.jang.shj@gmail.com>
2020-06-27 15:00:01 +0900

    Feat - Integrate operation argument - query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -11,13 +11,18 @@ const Query = {
 
     if (args.query) {
       opArgs.where = {
-        name_contains: args.query,
+        OR: [{ name_contains: args.query }, { email_contains: args.query }],
       }
     }
     return prisma.query.users(opArgs, info)
   },
   posts(parent, args, { prisma }, info) {
     const opArgs = {}
+    if (args.query) {
+      opArgs.where = {
+        OR: [{ title_contains: args.query }, { body_contains: args.query }],
+      }
+    }
     return prisma.query.posts(opArgs, info)
   },
   me(parent, args, { prisma, request }, info) {
