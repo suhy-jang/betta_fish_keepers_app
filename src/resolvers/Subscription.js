0a391eee626b43c5045989a594a092a958563557
shjang7 <suhy.jang.shj@gmail.com>
2020-06-28 16:25:17 +0900

    Feat - Add post subscription

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -31,6 +31,24 @@ const Subscription = {
       )
     },
   },
+  myPost: {
+    subscribe(parent, args, { prisma, request }, info) {
+      const userId = getUserId(request)
+
+      return prisma.subscription.post(
+        {
+          where: {
+            node: {
+              author: {
+                id: userId,
+              },
+            },
+          },
+        },
+        info,
+      )
+    },
+  },
 }
 
 export { Subscription as default }
