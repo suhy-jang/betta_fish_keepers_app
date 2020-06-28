349322e06ca5a4ceaa0cb88b3a81ee51df14a335
shjang7 <suhy.jang.shj@gmail.com>
2020-06-28 16:15:36 +0900

    Chore - Clean up edge case for user search

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -10,9 +10,7 @@ const Query = {
     }
 
     if (args.query) {
-      opArgs.where = {
-        OR: [{ name_contains: args.query }, { email_contains: args.query }],
-      }
+      opArgs.where = { name_contains: args.query }
     }
     return prisma.query.users(opArgs, info)
   },
