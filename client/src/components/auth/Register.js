80bbff6152db40da6d083b52388abc015d822d3b
shjang7 <suhy.jang.shj@gmail.com>
2020-07-08 19:50:26 +0900

    Feat - auth redux, register action

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -3,8 +3,9 @@ import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 import { setAlert } from '../../actions/alert'
+import { register } from '../../actions/auth'
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -22,7 +23,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert('Passwords do not match', 'danger')
     } else {
-      console.log(formData)
+      register({ name, email, password })
     }
   }
   return (
@@ -89,6 +90,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 }
 
-export default connect(null, { setAlert })(Register)
+export default connect(null, { setAlert, register })(Register)
---
+++ client/src/reducers/auth.js
@@ -0,0 +1,33 @@
+import { REGISTER_SUCCESS, REGISTER_FAILURE } from '../actions/types'
+
+const initialState = {
+  token: null,
+  isAuthenticated: false,
+  loading: false,
+  user: null,
+}
+
+export default function(state = initialState, action) {
+  const { type, payload } = action
+
+  switch (type) {
+    case REGISTER_SUCCESS:
+      localStorage.setItem('token', payload.token)
+      return {
+        ...state,
+        ...payload,
+        isAuthenticated: true,
+        loading: false,
+      }
+    case REGISTER_FAILURE:
+      localStorage.removeItem('token')
+      return {
+        ...state,
+        token: null,
+        isAuthenticated: false,
+        loading: false,
+      }
+    default:
+      return state
+  }
+}
