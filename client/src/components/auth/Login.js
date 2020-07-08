67761ce7d56018c49e68774eb31cf11581bfceef
shjang7 <suhy.jang.shj@gmail.com>
2020-07-08 18:33:13 +0900

    Feat - login form

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,8 +1,60 @@
-import React from 'react'
+import React, { Fragment, useState } from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const Login = props => {
-  return <div>Login</div>
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  })
+
+  const { email, password } = formData
+
+  const onChange = e => {
+    e.preventDefault()
+    setFormData({ ...formData, [e.target.name]: e.target.value })
+  }
+
+  const onSubmit = e => {
+    e.preventDefault()
+    console.log('Success at login')
+  }
+
+  return (
+    <Fragment>
+      <h1 className="large text-primary">Sign In</h1>
+      <p className="lead">
+        <i className="fas fa-user" /> Sign into your account
+      </p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input
+            type="email"
+            placeholder="Email Address"
+            name="email"
+            value={email}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            minlength="6"
+            name="password"
+            value={password}
+            onChange={e => onChange(e)}
+            required
+          />
+        </div>
+        <input type="submit" value="Login" className="btn btn-primary" />
+      </form>
+      <p className="my-1">
+        Don't have an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </Fragment>
+  )
 }
 
 Login.propTypes = {}
