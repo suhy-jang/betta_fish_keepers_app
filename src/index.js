2884e75d78b0e1f023aa7342f2e049273ae4a888
shjang7 <suhy.jang.shj@gmail.com>
2020-07-08 17:49:58 +0900

    Prod - Prepare deploy for server, client concurrently

--- src/index.js
+++ src/index.js
@@ -1,6 +1,38 @@
 import '@babel/polyfill/noConflict'
 import server from './server'
+import os from 'os'
+const path = require('path')
+const express = require('express')
 
-server.start({ port: process.env.PORT || 4000 }, () => {
+const host =
+  'http://' +
+  (process.env.NODE_ENV === 'production' ? os.hostname() : 'localhost')
+const port = process.env.PORT || 4000
+
+const opts = {
+  endpoint: '/graphql',
+  port,
+  tracing: true,
+  playground: '/graphql/playground',
+  cors: {
+    origin: [host + ':3000'],
+  },
+}
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  server.express.use('/', express.static('public'))
+  server.express.get('*', (req, res, next) => {
+    const routes = ['/graphql', '/subscriptions', '/graphql/playground']
+
+    if (routes.includes(req.url)) {
+      return next()
+    }
+
+    res.sendFile(path.resolve(__dirname, '../public', 'index.html'))
+  })
+}
+
+server.start(opts, () => {
   console.log('The server is up')
 })
