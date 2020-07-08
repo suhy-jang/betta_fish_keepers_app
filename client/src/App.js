f9de8a7ecd187c4936b76828dcc47417764fb980
shjang7 <suhy.jang.shj@gmail.com>
2020-07-08 13:20:56 +0900

    Feat - Set router for register, login

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,26 @@
 import React, { Fragment } from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from './components/layouts/NavBar'
 import Landing from './components/layouts/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
 import './App.css'
 
 const App = () => (
-  <Fragment>
-    <NavBar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component={Landing} />
+        <section className="container">
+          <Switch>
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </Switch>
+        </section>
+      </Switch>
+    </Fragment>
+  </Router>
 )
 
 export default App
